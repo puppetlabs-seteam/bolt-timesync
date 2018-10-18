9f9f76fd60b5afd15a539d4e1de08a64aa788127
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-10-18 15:46:47 +0200

    Update timesync.ps1

--- Boltdir/modules/tools/tasks/timesync.ps1
+++ Boltdir/modules/tools/tasks/timesync.ps1
@@ -3,10 +3,18 @@ Param(
   [Parameter(Mandatory = $True)] [Boolean] $Restart
 )
 
-w32tm /config /manualpeerlist:"0.nl.pool.ntp.org 1.nl.pool.ntp.org"
-w32tm /config /update
-w32tm /query /peers
+echo "Reconfiguring W32Time..."
+w32tm /config /syncfromflags:MANUAL /manualpeerlist:"0.nl.pool.ntp.org 1.nl.pool.ntp.org" /update
+echo ""
+echo "Resyncing clock..."
 w32tm /resync
+echo ""
+echo "Current time source:"
+w32tm /query /source
+echo ""
+echo "All configured time sources:"
+w32tm /query /peers
+
 if ($Restart) {
     write-host "Restart parameter enabled, restarting Windows Time service"
     restart-service 'W32Time' -force
