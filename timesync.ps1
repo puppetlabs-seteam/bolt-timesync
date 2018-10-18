17079fa7f91d8cbe663bde08eb6da60109f3ffa9
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-10-18 15:46:17 +0200

    Update timesync.ps1

--- timesync.ps1
+++ timesync.ps1
@@ -1,4 +1,11 @@
-w32tm /config /manualpeerlist:"0.nl.pool.ntp.org 1.nl.pool.ntp.org"
-w32tm /config /update
-w32tm /query /peers
+echo "Reconfiguring W32Time..."
+w32tm /config /syncfromflags:MANUAL /manualpeerlist:"0.nl.pool.ntp.org 1.nl.pool.ntp.org" /update
+echo ""
+echo "Resyncing clock..."
 w32tm /resync
+echo ""
+echo "Current time source:"
+w32tm /query /source
+echo ""
+echo "All configured time sources:"
+w32tm /query /peers
