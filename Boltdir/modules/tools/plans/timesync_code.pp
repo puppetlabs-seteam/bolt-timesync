1a969f7aab6f5174c88fc2dc992b7562a095df48
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-11-30 15:55:46 +0100

    Update timesync_code.pp

--- Boltdir/modules/tools/plans/timesync_code.pp
+++ Boltdir/modules/tools/plans/timesync_code.pp
@@ -5,10 +5,10 @@ plan tools::timesync_code(
 
   apply($nodes) {
     class { 'windowstime':
-      servers  => { '0.nl.pool.ntp.org' => '0x09',
-                    '1.nl.pool.ntp.org' => '0x09',
-                    '2.nl.pool.ntp.org' => '0x09',
-                    '3.nl.pool.ntp.org' => '0x09' }
+      servers  => { '0.nl.pool.ntp.org' => '0x08',
+                    '1.nl.pool.ntp.org' => '0x08',
+                    '2.nl.pool.ntp.org' => '0x08',
+                    '3.nl.pool.ntp.org' => '0x08' }
     }
   }
 }
