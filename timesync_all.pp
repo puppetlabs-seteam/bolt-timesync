7e67f16a4bcd2a5897cbacdfaf7446c6fd8bde6d
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-01-04 15:04:46 +0100

    bugfix

--- timesync_all.pp
+++ timesync_all.pp
@@ -3,7 +3,7 @@ case $facts['kernel'] {
     class { 'ntp':
       servers  => [ '0.nl.pool.ntp.org',
                     '1.nl.pool.ntp.org',
-                    '2.nl.pool.ntp.org' ]
+                    '2.nl.pool.ntp.org' ],
       restrict => [ '127.0.0.1' ],
     }
   }
