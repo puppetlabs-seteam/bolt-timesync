1a4711e3e1c2ee78b18cbaebe70afef5fd9af47c
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-01-07 15:09:11 +0000

    Update timesync.pp

--- Boltdir/modules/tools/plans/timesync.pp
+++ Boltdir/modules/tools/plans/timesync.pp
@@ -2,5 +2,5 @@ plan tools::timesync(
   TargetSpec $nodes,
 ) {
   run_task('tools::timesync', $nodes, restart => false)
-  run_task('service', $nodes, name => 'W32Time', action => 'restart')
+  run_task('service::windows', $nodes, name => 'W32Time', action => 'restart')
 }
